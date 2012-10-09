461b078a1d10adaf31d3703e493a1720808121be
Stephen Li <stephenl@codeaurora.org>
2012-10-09 14:51:54 -0700

    added qmi library in hal for Atlas
    Changed include path to QMI headers
    
    Change-Id: I999e1ed007ce91987105fdb2acdaa0cf5f2a5f12

--- loc_api/loc_api_v02/Android.mk
+++ loc_api/loc_api_v02/Android.mk
@@ -4,6 +4,13 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+# Define all platforms that have ATLAS. This will decide where we pick up
+# QMI Framework from. Atlas platforms will pick up the QMI Framework from
+# the Griffon binary, while other platforms will pick up the QMI Framework
+# provided by CoreBSP.
+ATLAS_PLATFORM_LIST := msm7627a
+
+
 LOCAL_MODULE := libloc_api_v02
 
 LOCAL_MODULE_TAGS := optional
@@ -11,11 +18,22 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SHARED_LIBRARIES := \
     libutils \
     libcutils \
-    libqmi_cci \
-    libqmi_csi \
-    libqmi_common_so \
     libloc_adapter \
     libgps.utils
+
+# If not compiling for Atlas, pick up CoreBsp QMI framework
+ifneq ($(call is-board-platform-in-list,$(ATLAS_PLATFORM_LIST)),true)
+LOCAL_SHARED_LIBRARIES += \
+    libqmi_cci \
+    libqmi_csi \
+    libqmi_common_so
+endif
+
+# If compiling for Atlas, pick up the QMI Framework in Griffon
+ifeq ($(call is-board-platform-in-list,$(ATLAS_PLATFORM_LIST)),true)
+LOCAL_SHARED_LIBRARIES += \
+    libqmi_client_griffon
+endif
 
 LOCAL_SRC_FILES += \
     LocApiV02Adapter.cpp \
@@ -31,9 +49,23 @@ LOCAL_CFLAGS += \
 ## Includes
 LOCAL_C_INCLUDES := \
     $(TARGET_OUT_HEADERS)/libloc_eng \
-    $(TARGET_OUT_HEADERS)/qmi-framework/inc \
-    $(TARGET_OUT_HEADERS)/qmi/inc \
     $(TARGET_OUT_HEADERS)/gps.utils
+
+# If not compiling for Atlas, setup include path to get QMI defs from
+# corebsp
+ifneq ($(call is-board-platform-in-list,$(ATLAS_PLATFORM_LIST)),true)
+LOCAL_C_INCLUDES += \
+    $(TARGET_OUT_HEADERS)/qmi-framework/inc \
+    $(TARGET_OUT_HEADERS)/qmi/inc
+endif
+
+# If compiling for Atlas, setup include path to get QMI defs from GPS branch
+# of corebsp QmiFW.
+ifeq ($(call is-board-platform-in-list,$(ATLAS_PLATFORM_LIST)),true)
+LOCAL_C_INCLUDES += \
+    $(TARGET_OUT_HEADERS)/qmi-framework-gps/inc
+endif
+
 
 LOCAL_PRELINK_MODULE := false
 
