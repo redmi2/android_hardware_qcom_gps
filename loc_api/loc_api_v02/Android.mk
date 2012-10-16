b242c20231346fd374ebb81b3f2a30dec69e42d4
Sridhar Gujje <sgujje@codeaurora.org>
2012-10-16 17:53:09 +0530

    Revert "griffon: add griffon qmi library in hal"
    This reverts commit d0012405ccab8e6f49407d4f893f6d2774dda114.

--- loc_api/loc_api_v02/Android.mk
+++ loc_api/loc_api_v02/Android.mk
@@ -4,9 +4,6 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-ATLAS_PLATFORM_LIST := msm7627a
-
-
 LOCAL_MODULE := libloc_api_v02
 
 LOCAL_MODULE_TAGS := optional
@@ -14,21 +11,11 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SHARED_LIBRARIES := \
     libutils \
     libcutils \
-    libloc_adapter \
-    libgps.utils
-
-ifneq ($(call is-board-platform-in-list,$(ATLAS_PLATFORM_LIST)),true)
-LOCAL_SHARED_LIBRARIES += \
     libqmi_cci \
     libqmi_csi \
-    libqmi_common_so
-endif
-
-
-ifeq ($(call is-board-platform-in-list,$(ATLAS_PLATFORM_LIST)),true)
-LOCAL_SHARED_LIBRARIES += \
-    libqmi_client_griffon
-endif
+    libqmi_common_so \
+    libloc_adapter \
+    libgps.utils
 
 LOCAL_SRC_FILES += \
     LocApiV02Adapter.cpp \
@@ -43,20 +30,9 @@ LOCAL_CFLAGS += \
 
 ## Includes
 LOCAL_C_INCLUDES := \
+    $(TARGET_OUT_HEADERS)/qmi-framework/inc \
     $(TARGET_OUT_HEADERS)/libloc_eng \
     $(TARGET_OUT_HEADERS)/gps.utils
-
-ifneq ($(call is-board-platform-in-list,$(ATLAS_PLATFORM_LIST)),true)
-LOCAL_C_INCLUDES += \
-    $(TARGET_OUT_HEADERS)/qmi-framework/inc
-endif
-
-
-ifeq ($(call is-board-platform-in-list,$(ATLAS_PLATFORM_LIST)),true)
-LOCAL_C_INCLUDES += \
-    $(TOP)/vendor/qcom/proprietary/gps/griffon/qmi_framework/inc
-endif
-
 
 LOCAL_PRELINK_MODULE := false
 
