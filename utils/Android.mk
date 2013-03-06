e163100ec3a8eac56568e1b5784cc17ab2fbb2b2
Kevin Tang <zhikait@codeaurora.org>
2013-03-06 09:53:38 -0800

    Added functionality for X509 certificate DER to PEM conversion
    Added utility functions to gps.utils library and test
    applications for converting X509 certificates from DER
    to PEM.
    
    Test applications are not built by default.
    
    Change-Id: I2be1727ef9932c6d5aa30560971bcf5af955a379

--- utils/Android.mk
+++ utils/Android.mk
@@ -1,22 +1,25 @@
-ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
-ifneq ($(BUILD_TINY_ANDROID),true)
-#Compile this library only for builds with the latest modem image
-
 LOCAL_PATH := $(call my-dir)
 
+ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
+ifneq ($(BUILD_TINY_ANDROID),true)
+
+#Compile this library only for builds with the latest modem image
 include $(CLEAR_VARS)
 
 ## Libs
 LOCAL_SHARED_LIBRARIES := \
     libutils \
-    libcutils
+    libcutils \
+    libssl \
+    libcrypto \
 
 LOCAL_SRC_FILES += \
     loc_log.cpp \
     loc_cfg.cpp \
     msg_q.c \
     linked_list.c \
-    loc_target.cpp
+    loc_target.cpp \
+    der2pem.c \
 
 LOCAL_CFLAGS += \
      -fno-short-enums \
@@ -25,7 +28,8 @@ LOCAL_CFLAGS += \
 LOCAL_LDFLAGS += -Wl,--export-dynamic
 
 ## Includes
-LOCAL_C_INCLUDES:=
+LOCAL_C_INCLUDES := \
+  external/openssl/include \
 
 LOCAL_COPY_HEADERS_TO:= gps.utils/
 LOCAL_COPY_HEADERS:= \
@@ -34,16 +38,50 @@ LOCAL_COPY_HEADERS:= \
    log_util.h \
    linked_list.h \
    msg_q.h \
-   loc_target.h
+   loc_target.h \
+   der2pem.h \
 
 LOCAL_MODULE := libgps.utils
-
 LOCAL_MODULE_TAGS := optional
-
 LOCAL_PRELINK_MODULE := false
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
 include $(BUILD_SHARED_LIBRARY)
 endif # not BUILD_TINY_ANDROID
 endif # BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
+
+include $(CLEAR_VARS)
+LOCAL_MODULE      := der2pem.host
+LOCAL_MODULE_STEM := der2pem
+LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS := -Wall -Werror
+LOCAL_SRC_FILES   := \
+  der2pem.c \
+  der2pem_main.c \
+
+LOCAL_SHARED_LIBRARIES := \
+  libssl \
+
+LOCAL_C_INCLUDES := \
+  external/openssl/include \
+
+include $(BUILD_HOST_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE      := der2pem.target
+LOCAL_MODULE_STEM := der2pem
+LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS := -Wall -Werror
+LOCAL_SRC_FILES   := \
+  der2pem.c \
+  der2pem_main.c \
+
+LOCAL_SHARED_LIBRARIES := \
+  libssl \
+  libcrypto \
+
+LOCAL_C_INCLUDES := \
+  external/openssl/include \
+
+include $(BUILD_EXECUTABLE)
 
